cab0ea7a7d01783612d9ba9c86c26117004034f8
SAIKISHAN SINGH THAKUR <sai.k.tech@gmail.com>
2020-10-16 23:57:56 +0530

    sending

--- mybot.js
+++ mybot.js
@@ -1,6 +1,11 @@
 
 
 const venom = require('venom-bot');
+//const random = require('random-words');
+const inspirational = require('inspirational-quotes');
+const date = new Date();
+const hour = date.getHours();
+
 
 venom.create().then((client) => {
     //start the client here to use
@@ -9,14 +14,21 @@ venom.create().then((client) => {
     console.log(err);
 })
 
+let messageArrray = [
+    'will get to you soon :)',
+    'arey howlee ruk naa ;)',
+    'kya re khali peeli disturb karra ;)',
+    'wasup mama all good? how come you have pinged me??',
+    'asshole - finally pinged me huh'
+]
 
 const start = (client) => {
 
     client.onMessage((message) => {
-        if (message && !message.isGroupMsg) {
+        if (message && !message.isGroupMsg && (hour > 5 && hour < 13)) {
             console.log(message);
-            client.sendText(message.from, `$message.body} - will get to you soon :)` ).then((result) => {
-            //    console.log(result);
+            client.sendText(message.from, inspirational.getRandomQuote()).then((result) => {
+                //    console.log(result);
             }).catch((err) => {
                 console.log(err);
             })
